0fae07b60d429971f7c28eacd94924d1770d41d6
Matthieu Falce <falce.matthieu@gmail.com>
2016-04-16 22:13:24 +0200

    on peut presque separer les tiles du tileSheet

--- src/Assets.js
+++ src/Assets.js
@@ -3,8 +3,50 @@ var Assets = {
 	loader: new PIXI.loaders.Loader(),
 	loaded: false,
 	//character: 'assets/testCercle.png',
-	
-	loadSprites: function (onLoad) {
+	sprintSheetURL: "art/tiles.png",
+	tilesSheetCanvas: document.createElement("canvas"),
+	tileCanvas: document.createElement("canvas"),
+
+	img: null,
+	subImgs: [],
+
+	extractSubImage: function(x, y) {
+		var tmpCtx = Assets.tileCanvas.getContext("2d");
+		tmpCtx.drawImage(
+			Assets.tilesSheetCanvas,
+			x * Assets.tileSize,
+			y * Assets.tileSize,
+			Assets.tileSize, Assets.tileSize,
+			0, 0,
+			Assets.tileSize, Assets.tileSize
+			);
+		return Assets.tileCanvas.toDataURL("image/png");
+	},
+
+	separateSprintes: function() {
+		var tilesSheetCtx = Assets.tilesSheetCanvas.getContext("2d");
+		Assets.tilesSheetCanvas.width = Assets.img.width;
+		Assets.tilesSheetCanvas.height = Assets.img.height;
+		tilesSheetCtx.drawImage(Assets.img, 0, 0);
+
+		var walls = tilesInfos.walls[0];
+		for (var type in walls) {
+			var tile = Assets.extractSubImage(
+									walls[type].x, walls[type].x, Assets.img
+								) ;
+			console.log(tile);
+		};
+	},
+
+	loadSpritesSheet: function(){
+		Assets.img = new Image();
+		Assets.img.onload = function () {
+			Assets.separateSprintes ();
+		};
+		Assets.img.src = Assets.sprintSheetURL;
+	},
+
+	loadSprites: function () {
 		var that = this;
 
 		this.loader.add("broc", "tmp_art/broc.png");
@@ -17,4 +59,5 @@ var Assets = {
 
 };
 
-Assets.loadSprites();
+Assets.loadSpritesSheet();
+//Assets.loadSprites();
