b4db9c43899f3a435184b6f3bf4752ade785af05
Matthieu Falce <falce.matthieu@gmail.com>
2016-04-16 20:58:14 +0200

    changement taille renderer

--- src/Vue.js
+++ src/Vue.js
@@ -7,7 +7,7 @@ function depthCompare(a,b) {
 }
 
 function Vue (model, controler) {
-	this.renderer = PIXI.autoDetectRenderer(800, 600,{backgroundColor : 0xE42217});
+	this.renderer = PIXI.autoDetectRenderer(800, 800,{backgroundColor : 0xE42217});
 	document.body.appendChild(this.renderer.view);
 	this.model = model;
 	//this.controler = controler;
